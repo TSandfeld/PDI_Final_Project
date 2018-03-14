0deecb8ea6d46fe9d9a0224ed5bbca4da2dbd1cb
TSandfeld <thomas.sandfeld@gmail.com>
2018-03-14 14:27:53 +0100

    Third commit

--- EnterReceipt.js
+++ EnterReceipt.js
@@ -1,11 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, TextInput, Dimensions, Image } from 'react-native';
+import { StyleSheet, 
+  Text, 
+  View, 
+  Button,
+  TouchableHighlight, 
+  TextInput, 
+  Dimensions, 
+  Image,
+  FlatList,
+  Alert} from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+
+const realm = require('realm');
+
+const itemSchema = {
+  name: 'Item',
+  properties: {
+    name: 'string',
+    price: 'double',
+    quantity: 'int',
+  }
+}
+
+const receiptSchema = {
+  name: 'Receipt',
+  properties: {
+    date: 'string',
+    store: 'string',
+    items: 'Item[]',
+  }
+}
+
+var data = [{key: "0", name: "", price: 0.0, quantity: 0}, {key: "1", name: "", price: 0.0, quantity: 0}];
 
 export default class EnterReceipt extends React.Component {
   static navigationOptions = {
     title: "Enter Receipt"
   };
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      items: [{key: "0"},{key: "1"}]
+    };
+  
+  }
+
+  addEntry=()=>{
+    var index = this.state.items.length
+    data.push(index.toString());
+    console.log(this.state.items)
+  }
+
+  submitReceipt=()=>{
+
+  }
+
+  _onChange=(key, value)=>{
+    var index = parseInt(key);
+    data[index].name = value;
+  }
 
   render() {
     return (
@@ -14,7 +69,47 @@ export default class EnterReceipt extends React.Component {
           <Image style={styles.imageLogo} source={require('./Images/frontscreen.png')}>
           </Image>
         </View>
-        <View style={styles.fieldView}>
+
+        <View style={styles.storeDateContainer}>
+          <View style={styles.sameLineInput}>
+            <Text> Store </Text>
+            <TextInput underlineColorAndroid='transparent' style={styles.inputText}></TextInput> 
+          </View>
+          <View style={styles.sameLineInput}>
+            <Text> Date</Text>
+            <TextInput underlineColorAndroid='transparent' style={styles.inputText}></TextInput> 
+          </View>
+          <View style={styles.sameLineInput}>
+            <TouchableHighlight onPress={this.addEntry}>
+              <Text> Add Item </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+
+        <View style={styles.listContainer}>
+          <FlatList
+            data={this.state.items}
+            renderItem={({item}) =>
+                <View style={styles.sameLineInput}>
+                  <Text>Item</Text>
+                  <TextInput underlineColorAndroid='transparent' style={styles.inputText} 
+                    onChangeText={ (text) => this._onChange(item.key, text)}></TextInput>
+                  <TextInput underlineColorAndroid='transparent' style={styles.inputMoney}></TextInput>
+                  <Text>Kr.</Text>
+                  <TextInput underlineColorAndroid='transparent' style={styles.inputMoney}></TextInput>
+                  <Text>pcs.</Text>
+                </View>
+              }
+          />
+        </View>
+
+        <View style={styles.bottomContainer}>
+          <TouchableHighlight style={styles.submitButton}>
+            <Text> Submit </Text>
+          </TouchableHighlight>
+        </View>
+
+        {/* <View style={styles.fieldView}>
           <View style={styles.sameLineInput}>
             <Text> Store </Text>
             <TextInput underlineColorAndroid='transparent' style={styles.inputText}></TextInput> 
@@ -84,7 +179,7 @@ export default class EnterReceipt extends React.Component {
           <TouchableHighlight style={styles.buttons}>
             <Text>Done</Text>
           </TouchableHighlight>
-        </View>
+        </View> */}
       </View>
     );
   }
@@ -98,6 +193,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  storeDateContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: win.width,
+    marginBottom: 20,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: win.width,
+  },
+  bottomContainer: {
+    flex: 0,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: win.width,
+    height: 100,
+  },
+  submitButton: {
+    width: win.width,
+    height: 40,
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    width: win.width,
   },
   buttons: {
     flex: 0,
